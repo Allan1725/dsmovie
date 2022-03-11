6497437d78c915ab291eb8df7ce2c4f2a2f97387
Allan1725 <allansantos.3949@gmail.com>
2022-03-11 02:06:39 -0300

    erro na tabela está sem o Score

--- backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperior.demo;
+package com.devsuperior.dsmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DsmovieApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DsmovieApplication.class, args);
 	}
 
 }
